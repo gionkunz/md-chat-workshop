48a4587fd9fc636ea94f80b1e838c15f5c9a8c9f
Gion Kunz <gion.kunz@ctp.com>
2014-09-08 22:09:40 +0200

    Enhanced step scripts :-)

--- step.sh
+++ step.sh
@@ -4,6 +4,11 @@ if [ `git branch | sed -n '/\* /s///p'` != "workshop" ]; then
   git checkout -b workshop
 fi
 
-git add .
-git commit -m "My changes for step-$1"
+if [ "$2" = "-save" ]; then
+  git add .
+  git commit -m "My changes for step-$1"
+else
+  git checkout .
+fi
+
 git rebase step-$1
